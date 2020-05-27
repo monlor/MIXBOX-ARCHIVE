3adb437795ff04d9222e50734de0c37c0243f44a
Chaco Green <monlor@qq.com>
2020-05-27 15:52:50 +0800

    opt: 优化ttyd配置脚本

--- apps/ttyd/scripts/config.sh
+++ apps/ttyd/scripts/config.sh
@@ -8,9 +8,9 @@ echo "[${appinfo}]"
 readsh "启动${appname}服务[1/0] " "enable" "1"
 if [ "$enable" == '1' ]; then
     # Scripts Here
-    # readsh "请输入npc连接命令：" "connect_cmd" 
-    readsh "请输入${appname}端口号" "port" "4200"
-
+    readsh "请输入${appname}端口号" "port" "6000"
+    readsh "请输入${appname}外网访问配置[1/0]" "openport" "0"
+    readsh "重启${appname}服务[1/0] " "res" "1"
     [ "$res" = '1' -o -z "$res" ] && return 0
 else
     return 1
