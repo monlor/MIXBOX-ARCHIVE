89cdcfd863f774a131f531f5ec1c7e81ae750d90
Chaco Green <monlor@qq.com>
2020-05-27 15:49:27 +0800

    opt: 优化ttyd脚本格式

--- apps/ttyd/scripts/ttyd.sh
+++ apps/ttyd/scripts/ttyd.sh
@@ -2,32 +2,21 @@
 source /etc/mixbox/bin/base
 eval `mbdb export webshell`
 
-# port=6000
-port=$(mbdb get ${appname}.main.port) || port=6000
-binname="shellinabox"
+port=${port:-6000}
 
 start () {
 
-	result=$(ps | grep ${mbroot}/apps/${appname}/bin/${appname} | grep -v grep | wc -l)
-    if [ "$result" != '0' ];then
-		logsh "【$service】" "${appname}已经在运行！"
-		exit 1
-	fi
+	[ -n "$(pidof ${appname})" ] && logsh "【$service】" "${appname}已经在运行！" && exit 1
 	logsh "【$service】" "正在启动${appname}服务... "
-
-	if [ ! -f ${mbroot}/apps/${appname}/bin/${appname} ]; then
-		bincheck ${binname} 
-    	[ $? -eq 0 ] && ln -sf $(which $binname) ${mbroot}/apps/${appname}/bin/${appname} 
-	fi
 	open_port
-    write_firewall_start
+	write_firewall_start
 	daemon ${mbroot}/apps/${appname}/bin/${appname} -p ${port} ash
 	if [ $? -ne 0 ]; then
-            logsh "【$service】" "启动${appname}服务失败！"
-    else
-            logsh "【$service】" "启动${appname}服务完成！"
-            logsh "【$service】" "请在浏览器访问http://$lanip:${port}"
-    fi
+		logsh "【$service】" "启动${appname}服务失败！"
+	else
+		logsh "【$service】" "启动${appname}服务完成！"
+		logsh "【$service】" "请在浏览器访问http://$lanip:${port}"
+	fi
 
 }
 
@@ -43,8 +32,7 @@ stop () {
 
 status() {
 
-	result=$(pssh | grep ${mbroot}/apps/${appname}/bin/${appname} | grep -v grep | wc -l)
-	if [ "$result" == '0' ]; then
+	if [ -n "$(pidof ${mbroot}/apps/${appname}/bin/${appname})" ]; then
 		status="未运行|0"
 	else
 		status="运行端口号: ${port}|1"
