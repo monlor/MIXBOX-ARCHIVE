3e887cfdc658e2b4516a56080b97c7fd7074456e
monlor <me@monlor.com>
2022-04-18 18:51:26 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ logsh "【Tools】" "请按任意键安装工具箱(Ctrl + C 退出)."
 read answer
 #check root
 # [ "$USER" != "root" ] && logsh "【Tools】"  "请使用root用户安装工具箱！" && exit 1
-mburl="https://cdn.jsdelivr.net/gh/monlor/mbfiles"
+mburl="${MB_URL:-https://cdn.jsdelivr.net/gh/monlor/mbfiles}"
 mbtmp="/tmp/mbtmp"
 [ ! -d "${mbtmp}" ] && mkdir -p ${mbtmp}
 model=$(uname -ms | tr ' ' '_' | tr '[A-Z]' '[a-z]')
